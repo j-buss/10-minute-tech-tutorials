c12b57db8b2416cf4d3821c49aff71d393ac88a8
j-buss <jeremy.f.buss@gmail.com>
2019-09-06 18:48:07 -0500

    updated app_engine_test_curl

--- gcp/app-engine-basic/environment/app_engine_test_curl.sh
+++ gcp/app-engine-basic/environment/app_engine_test_curl.sh
@@ -5,15 +5,16 @@ echo ------------------
 echo Tutorial script to cycle through curl commands to put some load on an app-engine app
 echo ------------------
 echo $# arguments
-if [ $# -ne 3 ]; then
+if [ $# -lt 3 ]; then
     echo "illegal number of parameters"
-    echo "Argument: project_id number_of_loops curl_delay_seconds"
+    echo "Argument: project_id number_of_loops curl_delay_seconds route-optional"
 else
     project_id=$1
     number_of_loops=$2
     curl_delay_seconds=$3
+	route=${4:-}
     for ((i=1;i<=number_of_loops;i++)); do
-        curl -s https://$project_id.appspot.com > /dev/null
+        curl -s https://$project_id.appspot.com/$route > /dev/null
         sleep $curl_delay_seconds
     done
 fi

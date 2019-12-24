de0e09d6e2a6f4bd5872f68b0b311b5e4567f689
j-buss <jeremy.f.buss@gmail.com>
2019-12-23 20:28:56 -0600

    updated sklearn.metric and directory names

--- machine_learning/part_01/sklearn_logistic_regression/train.py
+++ machine_learning/part_01/sklearn_logistic_regression/train.py
@@ -1,20 +1,18 @@
-from sklearn import datasets
+from __future__ import print_function
+
+import numpy as np
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+
 import mlflow
 import mlflow.sklearn
 
 if __name__ == "__main__":
-    iris = datasets.load_iris()
-    X = iris.data
-    Y = iris.target
-    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.33, random_state=44)
-    logreg = LogisticRegression()
-    logreg.fit(X_train, y_train)
-    y_pred = logreg.predict(X_test)
-    score = logreg.accuracy_score(y_test, y_pred)
+    X = np.array([-2, -1, 0, 1, 2, 1]).reshape(-1, 1)
+    y = np.array([0, 0, 1, 1, 1, 0])
+    lr = LogisticRegression()
+    lr.fit(X, y)
+    score = lr.score(X, y)
     print("Score: %s" % score)
     mlflow.log_metric("score", score)
-    mlflow.sklearn.log_model(logreg, "model")
+    mlflow.sklearn.log_model(lr, "model")
     print("Model saved in run %s" % mlflow.active_run().info.run_uuid)

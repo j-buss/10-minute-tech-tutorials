c5fecfe05a225b2c4c42d2255d1a589efde0ee32
j-buss <jeremy.f.buss@gmail.com>
2020-01-06 16:19:10 -0600

    commit changes for creating pandas dataframe from bigquery result

--- python/plotly-intro/03_Simple_BigQuery/main.py
+++ python/plotly-intro/03_Simple_BigQuery/main.py
@@ -1,35 +1,32 @@
 import flask
 import dash
 import dash_html_components as html
+import pandas as pd
 
 from google.cloud import bigquery
 bigquery_client = bigquery.Client()
 
 server = flask.Flask(__name__)
 
-@server.route('/')
-def index():
-    # Define query_job and query object
-    query_job = bigquery_client.query(
-        """
-        SELECT 
-          COUNT(*) as Rec_Count 
-        FROM 
-          `bigquery-public-data.census_utility.fips_codes_all`
-        """
-    )
-    # Handle query_job result and return to flask to display
-    res = query_job.result()
-    for row in res:
-        output = "Record Count: " + str(row[0])
-    return output
+query = \
+    """
+    SELECT
+        COUNT(*) as Rec_Count 
+    FROM 
+        `bigquery-public-data.census_utility.fips_codes_all`
+    """
+df = pd.read_gbq(query)
 
 app = dash.Dash(
     __name__,
     server=server,
     routes_pathname_prefix='/dash/'
 )
-app.layout = html.Div("My Dash app")
+
+app.layout = html.Div([
+    dcc.Input(id='my-id', value=df['Rec_Count'].iloc[0], type='text'),
+    html.Div(id='my-div')
+])
 
 
 if __name__ == '__main__':

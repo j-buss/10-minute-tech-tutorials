546bf441d6e7052b789008b61793f069584ba950
j-buss <cloudshell-user@ip-10-0-189-211.us-east-2.compute.internal>
2021-02-16 03:08:21 +0000

    store data as timestamp

--- python/python-copy-webpage/01_Simple_Local/main.py
+++ python/python-copy-webpage/01_Simple_Local/main.py
@@ -1,5 +1,13 @@
 import requests
-url = 'https://www.roguefitness.com/stainless-steel-ohio-bar'
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+import datetime
+filename = datetime.datetime.utcnow().isoformat()
+url = [
+        {
+            'page':'https://www.roguefitness.com/stainless-steel-ohio-bar',
+            'name':'stainless'
+        }
+    ]'https://www.roguefitness.com/rogue-ohio-bar-black-oxide']
+headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 result = requests.get(url, headers=headers)
-print(result.content.decode())
+with open(filename, "w") as text_file:
+    text_file.write(result.content.decode())

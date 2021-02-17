76d3f94439c4c5ec59ff7d6738411ce855b232a7
j-buss <cloudshell-user@ip-10-0-131-121.us-east-2.compute.internal>
2021-02-17 03:01:44 +0000

    added data structure

--- python/python-copy-webpage/01_Simple_Local/main.py
+++ python/python-copy-webpage/01_Simple_Local/main.py
@@ -1,13 +1,16 @@
 import requests
 import datetime
-filename = datetime.datetime.utcnow().isoformat()
-url = [
-        {
-            'page':'https://www.roguefitness.com/stainless-steel-ohio-bar',
-            'name':'stainless'
-        }
-    ]'https://www.roguefitness.com/rogue-ohio-bar-black-oxide']
+import os
+
 headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
-result = requests.get(url, headers=headers)
-with open(filename, "w") as text_file:
-    text_file.write(result.content.decode())
+
+def downloadWebpage(url, filename):
+    result = requests.get(url, headers=headers)
+    with open(filename, "w") as text_file:
+        text_file.write(result.content.decode())
+
+if __name__ == "__main__":
+    filename = datetime.datetime.utcnow().isoformat()
+    url = 'https://www.roguefitness.com/stainless-steel-ohio-bar'
+    links = [{'url':'https://www.roguefitness.com/stainless-steel-ohio-bar','name':'Stainless Ohio Bard'},{'url':'https://www.roguefitness.com/the-ohio-bar-cerakote'}]
+    #downloadWebpage(url, filename)
